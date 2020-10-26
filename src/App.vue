7a5f41cffee659aa83303d58ffb66c672e7d5feb
LIAOZHONGREN <68318882+LIAOZHONGREN@users.noreply.github.com>
2020-10-26 14:13:26 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,42 @@
 <template>
   <div>
-    <h3>{{title}}</h3>
-    <Hook />
+    <h3>{{ title }}</h3>
+    <div class="test">
+      <h6>演示Ref</h6>
+      <Ref />
+    </div>
+    <div class="test">
+      <h6>演示Reactive</h6>
+      <Reactive />
+    </div>
+    <div class="test">
+      <h6>演示ToRefs</h6>
+      <ToRefs />
+    </div>
+    <div class="test">
+      <h6>演示LifeCycleHook</h6>
+      <LifeCycleHook />
+    </div>
+    <div class="test">
+      <h6>演示Watch</h6>
+      <Watch />
+    </div>
+    <div class="test">
+      <h6>演示hook</h6>
+      <Hook />
+    </div>
+    <div class="test">
+      <h6>演示"app.config.errorHandler"</h6>
+      <button @click="throw_">抛一个异常</button>
+    </div>
+    <div class="test">
+      <h6>演示自定义指令</h6>
+      <div v-insert="'我是insert指令插入的文本'"></div>
+    </div>
+    <div class="test">
+      <h6>演示自定义插件</h6>
+      <div>{{ pro }}{{ $plugin() }}</div>
+    </div>
   </div>
 </template>
 
@@ -16,7 +51,18 @@ import Hook from "./components/hook/Hook.vue";
 
 export default defineComponent({
   name: "App",
-  components: { Hook },
+  components: { Ref, Reactive, ToRefs, LifeCycleHook, Watch, Hook },
+  inject: ["pro"],
+  setup() {
+    const throw_ = () => {
+      throw new Error("...");
+    };
+    return { throw_ };
+  },
+  // mounted() {
+  //   const that: any = this;
+  //   that.$plugin();
+  // },
 });
 </script>
 
@@ -28,5 +74,17 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.test {
+  position: relative;
+  width: 100%;
+  padding: 5px 0;
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+  border-bottom: solid #2c3e50 1px;
 }
 </style>
