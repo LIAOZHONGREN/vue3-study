c40e3abecfb84a7877dbfa86a383ef812ba411fd
LIAOZHONGREN <68318882+LIAOZHONGREN@users.noreply.github.com>
2020-10-26 14:14:34 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,23 @@ const app = createApp(App)
 app.config.errorHandler = (err, vm, info) => {
     console.log('errorHandler')
 }
-app.config.warnHandler = function (msg, vm, trace) {
+app.config.warnHandler = (msg, vm, trace) => {
     console.log('warnHandler')
 }
 app.config.globalProperties.title = 'Vue3学习'
+//自定义指令
+app.directive('insert', {
+    mounted(el: Element, binding) {
+        el.textContent = binding.value
+    },
+})
+//自定义插件
+app.use({
+    install: (app, options) => {
+        console.log(options)//options:{ tips: '安装自定义插件' }
+        app.provide('pro', '自定义插件')
+        app.config.globalProperties.$plugin = () => { console.log('安装了自定义插件') }
+    }
+}, { tips: '安装自定义插件' })
+
 app.mount('#app')
